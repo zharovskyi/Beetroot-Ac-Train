e2a0b06300d25cf3392e8701a8c075f4f6a55269
oleh.zharovskyi <oleh.zharovskyi@raketech.com>
2020-06-15 20:02:41 +0300

    add;

---
+++ 06_Asynchronous_JavaScript/06_files/swapi/swapi.js
@@ -0,0 +1,12 @@
+// https://swapi.co/documentation
+
+// #region Setup
+const URL = "http://swapi.dev/api/";
+const output = document.getElementById("output");
+const spinner = document.getElementById('spinner');
+
+output.innerHTML = "Loading ...";
+//#endregion
+
+
+
